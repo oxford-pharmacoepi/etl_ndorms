eeaa282591625c5aab3fd81124a14185dc55630a
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2023-10-13 11:13:11 +0100

    aesthetic improvements
    Alignment, case, data type names

--- sql_scripts/2b_aurum_lookup_create.sql
+++ sql_scripts/2b_aurum_lookup_create.sql
@@ -7,65 +7,65 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.gender
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.jobcat
 (
 	jobcatid integer NOT NULL,
-	description character varying(100) NOT NULL
+	description varchar(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.numunit
 (
 	numunitid integer NOT NULL,
-	description character varying(100) NULL
+	description varchar(100) NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.quantunit
 (
 	quantunitid integer NOT NULL,
-	description character varying(50) NOT NULL
+	description varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.refservicetype
 (
 	refservicetypeid integer NOT NULL,
-	description character varying(50) NOT NULL
+	description varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.region
 (
 	regionid integer NOT NULL,
-	description character varying(30) NOT NULL
+	description varchar(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.medicaldictionary
 (
 	medcodeid bigint NOT NULL,
 	observations bigint,
-	originalreadcode character varying(25),
-	cleansedreadcode character varying(10),
-	term character varying(265),
-	snomedctconceptid character varying(20),
-	snomedctdescriptionid character varying(20),
-	release character varying(1),
+	originalreadcode varchar(25),
+	cleansedreadcode varchar(10),
+	term varchar(265),
+	snomedctconceptid varchar(20),
+	snomedctdescriptionid varchar(20),
+	release varchar(1),
 	emiscodecategoryid smallint
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.productdictionary
 (
 	prodcodeid bigint NOT NULL,
-	dmdid character varying(20),
-	termfromemis character varying(250),
-	productname character varying(250),
-	formulation character varying(250),
-	routeofadministration character varying(100),
-	drugsubstancename character varying(1000),
-	substancestrength character varying(650),
-	bnfchapter character varying(200),
+	dmdid varchar(20),
+	termfromemis varchar(250),
+	productname varchar(250),
+	formulation varchar(250),
+	routeofadministration varchar(100),
+	drugsubstancename varchar(1000),
+	substancestrength varchar(650),
+	bnfchapter varchar(200),
 	drugissues bigint NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.VisionToEmisMigrators
 (
-	gold_pracid		INT NOT NULL,
-	gold_lcdate		DATE NOT NULL,
-	emis_pracid		INT NOT NULL,
-	emis_joindate	DATE NOT NULL,
-	emis_fdcdate	DATE NOT NULL
+	gold_pracid		integer NOT NULL,
+	gold_lcdate		date NOT NULL,
+	emis_pracid		integer NOT NULL,
+	emis_joindate	date NOT NULL,
+	emis_fdcdate	date NOT NULL
 );
