f6e37fc26672f10837c66758c41dd93d7e2611c7
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2023-10-30 17:43:55 +0000

    Update 5a_cdm_pk_idx_metadata.sql

--- sql_scripts/5a_cdm_pk_idx_metadata.sql
+++ sql_scripts/5a_cdm_pk_idx_metadata.sql
@@ -1,3 +1,3 @@
-ALTER TABLE {TARGET_SCHEMA}.METADATA ADD CONSTRAINT xpk_METADATA PRIMARY KEY (metadata_concept_id);
+ALTER TABLE {TARGET_SCHEMA}.metadata ADD CONSTRAINT xpk_METADATA PRIMARY KEY (metadata_concept_id);
 CREATE INDEX idx_metadata_concept_id_1 ON {TARGET_SCHEMA}.metadata (metadata_concept_id ASC);
 CLUSTER {TARGET_SCHEMA}.metadata USING idx_metadata_concept_id_1;
