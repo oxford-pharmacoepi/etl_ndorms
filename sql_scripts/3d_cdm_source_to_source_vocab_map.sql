f1ff054b4f465d97341f33d6715ad4dc2bd12f1c
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2023-10-30 17:45:38 +0000

    Changed {TARGET_SCHEMA} to {VOCABULARY_SCHEMA}
    Changed {TARGET_SCHEMA} to {VOCABULARY_SCHEMA} to allow customization

--- sql_scripts/3d_cdm_source_to_source_vocab_map.sql
+++ sql_scripts/3d_cdm_source_to_source_vocab_map.sql
@@ -1,4 +1,4 @@
-drop table if exists {TARGET_SCHEMA}.source_to_source_vocab_map CASCADE;
+drop table if exists {VOCABULARY_SCHEMA}.source_to_source_vocab_map CASCADE;
 
 WITH CTE_VOCAB_MAP AS (
        SELECT c.concept_code AS SOURCE_CODE, c.concept_id AS SOURCE_CONCEPT_ID, c.CONCEPT_NAME AS SOURCE_CODE_DESCRIPTION,
@@ -13,7 +13,7 @@ WITH CTE_VOCAB_MAP AS (
                                         c1.VALID_START_DATE AS SOURCE_VALID_START_DATE, c1.VALID_END_DATE AS SOURCE_VALID_END_DATE,stcm.INVALID_REASON AS SOURCE_INVALID_REASON,
                                         target_concept_id, c2.CONCEPT_NAME AS TARGET_CONCEPT_NAME, target_vocabulary_id, c2.domain_id AS TARGET_DOMAIN_ID, c2.concept_class_id AS TARGET_CONCEPT_CLASS_ID,
                      c2.INVALID_REASON AS TARGET_INVALID_REASON, c2.standard_concept AS TARGET_STANDARD_CONCEPT
-       FROM {TARGET_SCHEMA}.source_to_concept_map stcm
+       FROM {VOCABULARY_SCHEMA}.source_to_concept_map stcm
               LEFT OUTER JOIN {VOCABULARY_SCHEMA}.CONCEPT c1
                      ON c1.concept_id = stcm.source_concept_id
               LEFT OUTER JOIN {VOCABULARY_SCHEMA}.CONCEPT c2
@@ -21,7 +21,7 @@ WITH CTE_VOCAB_MAP AS (
        WHERE stcm.INVALID_REASON IS NULL
 )
 
-SELECT * INTO {TARGET_SCHEMA}.source_to_source_vocab_map FROM CTE_VOCAB_MAP;
+SELECT * INTO {VOCABULARY_SCHEMA}.source_to_source_vocab_map FROM CTE_VOCAB_MAP;
 
-create index idx_source_vocab_map_source_code on {TARGET_SCHEMA}.source_to_source_vocab_map (source_code);
-create index idx_source_vocab_map_source_vocab_id on {TARGET_SCHEMA}.source_to_source_vocab_map (source_vocabulary_id);
+create index idx_source_vocab_map_source_code on {VOCABULARY_SCHEMA}.source_to_source_vocab_map (source_code);
+create index idx_source_vocab_map_source_vocab_id on {VOCABULARY_SCHEMA}.source_to_source_vocab_map (source_vocabulary_id);
