8841bcca298f7f51a1aaf723865626f601531d76
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2023-11-07 11:45:16 +0000

    added missing FKs

--- sql_scripts/5a_cdm_pk_idx_death.sql
+++ sql_scripts/5a_cdm_pk_idx_death.sql
@@ -1,1 +1,4 @@
 ALTER TABLE {TARGET_SCHEMA}.death ADD CONSTRAINT xpk_death PRIMARY KEY (person_id);
+
+CREATE INDEX idx_death_person_id_1  ON {TARGET_SCHEMA}.death  (person_id ASC);	-- Added by Teen
+CLUSTER {TARGET_SCHEMA}.death  USING idx_death_person_id_1 ; 					-- Added by Teen
--- sql_scripts/5a_cdm_pk_idx_provider.sql
+++ sql_scripts/5a_cdm_pk_idx_provider.sql
@@ -1,1 +1,4 @@
 ALTER TABLE {TARGET_SCHEMA}.provider ADD CONSTRAINT xpk_provider PRIMARY KEY (provider_id);
+
+CREATE INDEX idx_provider_id_1  ON {TARGET_SCHEMA}.provider  (provider_id ASC); -- Added by Teen
+CLUSTER {TARGET_SCHEMA}.provider  USING idx_provider_id_1 ;						-- Added by Teen
