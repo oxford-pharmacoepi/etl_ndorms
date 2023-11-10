cf8ab0444ab85eb3ef62e8b5ced40472ad141fe9
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2023-11-10 15:02:03 +0000

    Added for OMOP 5.4
    Added for OMOP 5.4, but compatible with 5.3

--- sql_scripts/5b_cdm_fk_visit_detail__concept.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_concept FOREIGN KEY (visit_detail_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_type_concept FOREIGN KEY (visit_detail_type_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_discharge FOREIGN KEY (discharge_to_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_concept_s FOREIGN KEY (visit_detail_source_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
--- sql_scripts/5b_cdm_fk_visit_detail__visit_detail.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_preceding FOREIGN KEY (preceding_visit_detail_id) REFERENCES {TARGET_SCHEMA}.visit_detail (visit_detail_id);
-ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_parent FOREIGN KEY (visit_detail_parent_id) REFERENCES {TARGET_SCHEMA}.visit_detail (visit_detail_id);
