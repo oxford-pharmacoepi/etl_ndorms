30865124cac9a3d3c68ad332a6f25eeadcb9e626
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2023-11-10 15:24:00 +0000

    Update 5b4_cdm_fk_visit_detail__concept.sql

--- sql_scripts/5b4_cdm_fk_visit_detail__concept.sql
+++ sql_scripts/5b4_cdm_fk_visit_detail__concept.sql
@@ -2,5 +2,4 @@ ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_concept FOR
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_type_concept FOREIGN KEY (visit_detail_type_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_discharged FOREIGN KEY (discharged_to_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_concept_s FOREIGN KEY (visit_detail_source_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
-
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT fpk_v_detail_admitted FOREIGN KEY (admitted_from_concept_id) REFERENCES {VOCABULARY_SCHEMA}.concept (concept_id);
