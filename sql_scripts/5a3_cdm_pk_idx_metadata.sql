be5685a6b5d9b1039f8ffe529e84e64335fb09bf
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-01-11 15:30:56 +0000

    Added tablespace

--- sql_scripts/5a3_cdm_pk_idx_metadata.sql
+++ sql_scripts/5a3_cdm_pk_idx_metadata.sql
@@ -1,3 +1,3 @@
-ALTER TABLE {TARGET_SCHEMA}.metadata ADD CONSTRAINT xpk_METADATA PRIMARY KEY (metadata_concept_id);
-CREATE INDEX idx_metadata_concept_id_1 ON {TARGET_SCHEMA}.metadata (metadata_concept_id ASC);
+ALTER TABLE {TARGET_SCHEMA}.metadata ADD CONSTRAINT xpk_METADATA PRIMARY KEY (metadata_concept_id) USING INDEX TABLESPACE pg_default;
+CREATE INDEX idx_metadata_concept_id_1 ON {TARGET_SCHEMA}.metadata (metadata_concept_id ASC) TABLESPACE pg_default;
 CLUSTER {TARGET_SCHEMA}.metadata USING idx_metadata_concept_id_1;
