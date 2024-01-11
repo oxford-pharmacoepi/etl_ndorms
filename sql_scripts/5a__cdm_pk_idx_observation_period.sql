32d19a70947618e36cb8f9c6fe68709ab39013fd
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-01-11 15:43:13 +0000

    For data provided already mapped

---
+++ sql_scripts/5a__cdm_pk_idx_observation_period.sql
@@ -0,0 +1,3 @@
+ALTER TABLE {TARGET_SCHEMA}.observation_period ADD CONSTRAINT xpk_observation_period PRIMARY KEY (observation_period_id) USING INDEX TABLESPACE pg_default;
+CREATE INDEX idx_observation_period_id ON {TARGET_SCHEMA}.observation_period (person_id ASC) TABLESPACE pg_default;
+CLUSTER {TARGET_SCHEMA}.observation_period USING idx_observation_period_id;
