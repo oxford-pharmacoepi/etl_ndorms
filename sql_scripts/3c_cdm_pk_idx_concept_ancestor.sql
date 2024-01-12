9c21142717393dedbd102f0c7430c86af9a6d940
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-01-12 15:15:33 +0000

    Added tablespace

--- sql_scripts/3c_cdm_pk_idx_concept_ancestor.sql
+++ sql_scripts/3c_cdm_pk_idx_concept_ancestor.sql
@@ -1,6 +1,5 @@
-ALTER TABLE {VOCABULARY_SCHEMA}.CONCEPT_ANCESTOR ADD CONSTRAINT xpk_concept_ancestor PRIMARY KEY (ancestor_concept_id,descendant_concept_id);
+ALTER TABLE {VOCABULARY_SCHEMA}.CONCEPT_ANCESTOR ADD CONSTRAINT xpk_concept_ancestor PRIMARY KEY (ancestor_concept_id,descendant_concept_id) USING INDEX TABLESPACE pg_default;
 
-CREATE INDEX idx_concept_ancestor_id_1  ON {VOCABULARY_SCHEMA}.concept_ancestor  (ancestor_concept_id ASC);
-CLUSTER {VOCABULARY_SCHEMA}.concept_ancestor  USING idx_concept_ancestor_id_1 ;
-CREATE INDEX idx_concept_ancestor_id_2 ON {VOCABULARY_SCHEMA}.concept_ancestor (descendant_concept_id ASC);
-
+CREATE INDEX idx_concept_ancestor_id_1 ON {VOCABULARY_SCHEMA}.concept_ancestor (ancestor_concept_id ASC) TABLESPACE pg_default;
+CLUSTER {VOCABULARY_SCHEMA}.concept_ancestor USING idx_concept_ancestor_id_1;
+CREATE INDEX idx_concept_ancestor_id_2 ON {VOCABULARY_SCHEMA}.concept_ancestor (descendant_concept_id ASC) TABLESPACE pg_default;
--- sql_scripts/3c_cdm_pk_idx_concept_relationship.sql
+++ sql_scripts/3c_cdm_pk_idx_concept_relationship.sql
@@ -1,6 +1,6 @@
-ALTER TABLE {VOCABULARY_SCHEMA}.CONCEPT_RELATIONSHIP ADD CONSTRAINT xpk_concept_relationship PRIMARY KEY (concept_id_1,concept_id_2,relationship_id);
+ALTER TABLE {VOCABULARY_SCHEMA}.CONCEPT_RELATIONSHIP ADD CONSTRAINT xpk_concept_relationship PRIMARY KEY (concept_id_1,concept_id_2,relationship_id) USING INDEX TABLESPACE pg_default;
 
-CREATE INDEX idx_concept_relationship_id_1 ON {VOCABULARY_SCHEMA}.concept_relationship (concept_id_1 ASC);
-CLUSTER {VOCABULARY_SCHEMA}.concept_relationship  USING idx_concept_relationship_id_1 ;
-CREATE INDEX idx_concept_relationship_id_2 ON {VOCABULARY_SCHEMA}.concept_relationship (concept_id_2 ASC);
-CREATE INDEX idx_concept_relationship_id_3 ON {VOCABULARY_SCHEMA}.concept_relationship (relationship_id ASC);
+CREATE INDEX idx_concept_relationship_id_1 ON {VOCABULARY_SCHEMA}.concept_relationship (concept_id_1 ASC) TABLESPACE pg_default;
+CLUSTER {VOCABULARY_SCHEMA}.concept_relationship USING idx_concept_relationship_id_1;
+CREATE INDEX idx_concept_relationship_id_2 ON {VOCABULARY_SCHEMA}.concept_relationship (concept_id_2 ASC) TABLESPACE pg_default;
+CREATE INDEX idx_concept_relationship_id_3 ON {VOCABULARY_SCHEMA}.concept_relationship (relationship_id ASC) TABLESPACE pg_default;
