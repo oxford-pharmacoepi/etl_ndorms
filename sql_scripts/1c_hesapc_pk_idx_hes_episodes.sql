e606adb0be4287f2bc2553ef13f1653e40c912db
Mandickel Kamtengeni <41611068+Mandickel@users.noreply.github.com>
2024-01-16 15:15:47 +0000

    Update 1c_hesapc_pk_idx_hes_episodes.sql

--- sql_scripts/1c_hesapc_pk_idx_hes_episodes.sql
+++ sql_scripts/1c_hesapc_pk_idx_hes_episodes.sql
@@ -1,7 +1,7 @@
 --hes_episodes
 alter table {SOURCE_SCHEMA}.hes_episodes add constraint pk_hes_episodes primary key (patid, epikey) USING INDEX TABLESPACE pg_default;
-create index idx_hesapc_episodes_patid on {SOURCE_SCHEMA}.hes_episodes (patid, epikey);
+create index idx_hesapc_episodes_patid on {SOURCE_SCHEMA}.hes_episodes (patid, epikey) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hes_episodes using idx_hesapc_episodes_patid;
-create index idx_hesapc_episodes_patid_spno on {SOURCE_SCHEMA}.hes_episodes (patid, spno);
+create index idx_hesapc_episodes_patid_spno on {SOURCE_SCHEMA}.hes_episodes (patid, spno) TABLESPACE pg_default;
 
-create index idx_hesapc_pconsult on {SOURCE_SCHEMA}.hes_episodes (pconsult, tretspef, mainspef);
+create index idx_hesapc_pconsult on {SOURCE_SCHEMA}.hes_episodes (pconsult, tretspef, mainspef) TABLESPACE pg_default;
