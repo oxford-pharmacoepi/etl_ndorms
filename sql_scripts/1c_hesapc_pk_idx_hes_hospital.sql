36802a28de4972e29e84106e2e34af7d71d2fd32
Mandickel Kamtengeni <41611068+Mandickel@users.noreply.github.com>
2024-01-16 15:16:46 +0000

    Update 1c_hesapc_pk_idx_hes_hospital.sql

--- sql_scripts/1c_hesapc_pk_idx_hes_hospital.sql
+++ sql_scripts/1c_hesapc_pk_idx_hes_hospital.sql
@@ -1,4 +1,4 @@
 --hes_hospital
 alter table {SOURCE_SCHEMA}.hes_hospital add constraint pk_hes_hospital primary key (spno) USING INDEX TABLESPACE pg_default;
-create index idx_hesapc_hospital_patid on {SOURCE_SCHEMA}.hes_hospital (patid, spno);
+create index idx_hesapc_hospital_patid on {SOURCE_SCHEMA}.hes_hospital (patid, spno) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hes_hospital using idx_hesapc_hospital_patid;
