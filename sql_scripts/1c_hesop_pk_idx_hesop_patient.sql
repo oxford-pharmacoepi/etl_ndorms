32ca319f17f1d6076eeb02925f76725643cce3cc
Mandickel Kamtengeni <41611068+Mandickel@users.noreply.github.com>
2024-01-16 15:20:03 +0000

    Update 1c_hesop_pk_idx_hesop_patient.sql

--- sql_scripts/1c_hesop_pk_idx_hesop_patient.sql
+++ sql_scripts/1c_hesop_pk_idx_hesop_patient.sql
@@ -1,6 +1,6 @@
 --hesop_patient
 ALTER TABLE {SOURCE_SCHEMA}.hesop_patient ADD CONSTRAINT pk_hesop_patient PRIMARY KEY (patid, match_rank) USING INDEX TABLESPACE pg_default;
-create index idx_hesop_patient_patid on {SOURCE_SCHEMA}.hesop_patient(patid);
+create index idx_hesop_patient_patid on {SOURCE_SCHEMA}.hesop_patient(patid) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hesop_patient using idx_hesop_patient_patid;
-create index idx_hesop_patient_ethnicity on {SOURCE_SCHEMA}.hesop_patient(patid, gen_ethnicity);
+create index idx_hesop_patient_ethnicity on {SOURCE_SCHEMA}.hesop_patient(patid, gen_ethnicity) TABLESPACE pg_default;
 
