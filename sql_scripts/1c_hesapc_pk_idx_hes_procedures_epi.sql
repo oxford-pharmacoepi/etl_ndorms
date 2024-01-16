0217eff8666e10e1d812691bf6b754fa7b8083fc
Mandickel Kamtengeni <41611068+Mandickel@users.noreply.github.com>
2024-01-16 15:18:06 +0000

    Update 1c_hesapc_pk_idx_hes_procedures_epi.sql

--- sql_scripts/1c_hesapc_pk_idx_hes_procedures_epi.sql
+++ sql_scripts/1c_hesapc_pk_idx_hes_procedures_epi.sql
@@ -1,4 +1,4 @@
 --hes_procedures_epi
 alter table {SOURCE_SCHEMA}.hes_procedures_epi add constraint pk_hes_procedures_epi primary key (patid,epikey,p_order) USING INDEX TABLESPACE pg_default;
-create index idx_hesapc_procedures_epi_patid on {SOURCE_SCHEMA}.hes_procedures_epi(patid,epikey,p_order);
+create index idx_hesapc_procedures_epi_patid on {SOURCE_SCHEMA}.hes_procedures_epi(patid,epikey,p_order) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hes_procedures_epi using idx_hesapc_procedures_epi_patid;
