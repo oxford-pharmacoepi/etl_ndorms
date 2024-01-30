4d4a15bf79035e191dcf1b0c0777a49d2f40d2f9
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-01-30 13:05:09 +0000

    Update 3b_cdm_create_vocabulary.sql

--- sql_scripts/3b_cdm_create_vocabulary.sql
+++ sql_scripts/3b_cdm_create_vocabulary.sql
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.CONCEPT (
 			concept_code varchar(50) NOT NULL,
 			valid_start_date date NOT NULL,
 			valid_end_date date NOT NULL,
-			invalid_reason varchar(1) NULL );
 			invalid_reason varchar(1) NULL )
 			TABLESPACE pg_default;
 
@@ -17,21 +16,18 @@ CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.VOCABULARY (
 			vocabulary_name varchar(255) NOT NULL,
 			vocabulary_reference varchar(255) NULL,
 			vocabulary_version varchar(255) NULL,
-			vocabulary_concept_id integer NOT NULL );
 			vocabulary_concept_id integer NOT NULL )
 			TABLESPACE pg_default;
 
 CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.DOMAIN (
 			domain_id varchar(20) NOT NULL,
 			domain_name varchar(255) NOT NULL,
-			domain_concept_id integer NOT NULL );
 			domain_concept_id integer NOT NULL )
 			TABLESPACE pg_default;
 
 CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.CONCEPT_CLASS (
 			concept_class_id varchar(20) NOT NULL,
 			concept_class_name varchar(255) NOT NULL,
-			concept_class_concept_id integer NOT NULL );
 			concept_class_concept_id integer NOT NULL )
 			TABLESPACE pg_default;
 
@@ -51,7 +47,6 @@ CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.RELATIONSHIP (
 			is_hierarchical varchar(1) NOT NULL,
 			defines_ancestry varchar(1) NOT NULL,
 			reverse_relationship_id varchar(20) NOT NULL,
-			relationship_concept_id integer NOT NULL );
 			relationship_concept_id integer NOT NULL )
 			TABLESPACE pg_default;
 
@@ -64,7 +59,6 @@ CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.CONCEPT_ANCESTOR (
 			ancestor_concept_id integer NOT NULL,
 			descendant_concept_id integer NOT NULL,
 			min_levels_of_separation integer NOT NULL,
-			max_levels_of_separation integer NOT NULL );
 			max_levels_of_separation integer NOT NULL )
 			TABLESPACE pg_default;
 
@@ -100,7 +94,6 @@ CREATE TABLE IF NOT EXISTS {VOCABULARY_SCHEMA}.DRUG_STRENGTH (
 			box_size integer NULL,
 			valid_start_date date NOT NULL,
 			valid_end_date date NOT NULL,
-			invalid_reason varchar(1) NULL );
 			invalid_reason varchar(1) NULL )
 			TABLESPACE pg_default;
 
