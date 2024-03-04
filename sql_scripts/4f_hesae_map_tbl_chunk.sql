0ac42cc755a4a93257a5790f464e1f2a18d1381d
Mandickel Kamtengeni <mandickel@gmail.com>
2024-03-04 13:35:24 +0000

    Adding HESAE_Stem scripts & ETL Logic Documentation

---
+++ sql_scripts/4f_hesae_map_tbl_chunk.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA IF NOT EXISTS {CHUNK_SCHEMA};
+--------------------------------
+-- CHUNK_PERSON
+--------------------------------
+DROP TABLE IF EXISTS {CHUNK_SCHEMA}.chunk_person;
+
+CREATE TABLE {CHUNK_SCHEMA}.chunk_person TABLESPACE pg_default AS 
+	select (floor((row_number() over (order by person_id)-1)/{CHUNK_SIZE}) + 1)::int as chunk_id,
+	person_id
+	FROM {TARGET_SCHEMA}.person
+	order by chunk_id, person_id;
+
+ALTER TABLE {CHUNK_SCHEMA}.chunk_person ADD CONSTRAINT pk_chunk_person PRIMARY KEY (chunk_id, person_id) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX idx_hesae_chunk_person_id ON {CHUNK_SCHEMA}.chunk_person (chunk_id, person_id ASC) TABLESPACE pg_default;
+CLUSTER {CHUNK_SCHEMA}.chunk_person USING idx_hesae_chunk_person_id;
+
+--------------------------------
+-- CHUNK
+--------------------------------
+DROP TABLE IF EXISTS {CHUNK_SCHEMA}.chunk;
+
+CREATE TABLE {CHUNK_SCHEMA}.chunk TABLESPACE pg_default AS 
+SELECT distinct chunk_id,
+		null::varchar(20) as stem_source_tbl,
+		null::varchar(20) as stem_tbl,
+		null::bigint as stem_id_start,
+		null::bigint as stem_id_end,
+		0::smallint as completed 
+FROM {CHUNK_SCHEMA}.chunk_person;
+
+ALTER TABLE {CHUNK_SCHEMA}.chunk ADD CONSTRAINT pk_chunk PRIMARY KEY (chunk_id) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX idx_hesae_chunk_id ON {CHUNK_SCHEMA}.chunk (chunk_id ASC) TABLESPACE pg_default;
+CLUSTER {CHUNK_SCHEMA}.chunk USING idx_hesae_chunk_id;
+CREATE INDEX idx_hesae_chunk_completed ON {CHUNK_SCHEMA}.chunk (completed) TABLESPACE pg_default;
+
+--------------------------------
+-- _RECORDS
+--------------------------------
+drop table if exists {TARGET_SCHEMA}._records CASCADE;
+create table {TARGET_SCHEMA}._records (
+	tbl_name varchar(25) NOT NULL,
+	{TARGET_SCHEMA}_records bigint DEFAULT 0,
+	{TARGET_SCHEMA}_nok_records bigint DEFAULT 0,
+	total_records bigint DEFAULT 0
+) TABLESPACE pg_default;
+
+
+--------------------------------
+-- DROP TABLES CREATED BY CHUNKING
+--------------------------------
+truncate table {TARGET_SCHEMA}.condition_occurrence;
+truncate table {TARGET_SCHEMA}.procedure_occurrence;
+truncate table {TARGET_SCHEMA}.measurement;
+truncate table {TARGET_SCHEMA}.device_exposure;
+truncate table {TARGET_SCHEMA}.drug_exposure;
+truncate table {TARGET_SCHEMA}.observation;
+truncate table {TARGET_SCHEMA}.specimen;
---
+++ sql_scripts/4g_hesae_map_tbl_stem_source.sql
@@ -0,0 +1,115 @@
+--Insert into temp table from hesae_diagnosis
+CREATE TABLE IF NOT EXISTS {CHUNK_SCHEMA}.stem_source_{CHUNK_ID} (LIKE {TARGET_SCHEMA}.STEM_SOURCE) TABLESPACE pg_default;
+--Inserting Accident & Emergency diagnoses/ICD-10/Read coded diagnosis records into stem_source table from hesae_diagnosis
+WITH cte0 AS (
+    SELECT person_id
+    FROM {CHUNK_SCHEMA}.chunk_person
+	WHERE chunk_id = {CHUNK_ID}
+),
+cte1 AS (
+	SELECT 
+		t1.person_id,
+		NULL::INT AS provider_id,
+		t2.aekey::VARCHAR AS visit_source_value,
+		t2.aekey::VARCHAR AS visit_detail_source_value,
+		t3.arrivaldate AS start_date,
+		t3.arrivaldate AS end_date,
+		CASE
+			WHEN t2.DIAGSCHEME = 1 THEN CASE WHEN LENGTH(t2.diag) = 2 OR LENGTH(t2.diag) = 3 THEN t2.diag ELSE t2.diag END
+			WHEN t2.DIAGSCHEME = 2 THEN
+				CASE 
+					WHEN LENGTH(t2.diag) = 4 AND RIGHT(t2.diag, 1) IN ('X', '.') THEN LEFT(t2.diag, 3) 
+					WHEN LENGTH(t2.diag) = 4 THEN CONCAT(LEFT(t2.diag, 3), '.', RIGHT(t2.diag, 1))
+					ELSE t2.diag
+				END
+			WHEN t2.DIAGSCHEME = 4 THEN f.cleansedreadcode ELSE t2.diag
+		END AS source_value, 
+		CASE 
+			WHEN t2.diag_order = 1 THEN 32902
+			WHEN t2.diag_order > 1 THEN 32908
+			ELSE NULL::INT
+		END AS disease_status_concept_id,
+		t2.diag_order AS disease_status_source_value
+	FROM 
+		cte0 t1
+		INNER JOIN {SOURCE_SCHEMA}.hesae_diagnosis t2 ON t2.patid = t1.person_id
+		INNER JOIN {SOURCE_SCHEMA}.hesae_attendance t3 ON t2.patid = t3.patid AND t2.aekey = t3.aekey
+		LEFT JOIN source.medicaldictionary f ON LEFT(f.cleansedreadcode, 5) = LEFT(t2.diag, 5)
+	ORDER BY 
+		t1.person_id, t2.aekey, t2.diag_order
+),	
+cte2 as (
+		SELECT DISTINCT t4.*, t5.source_concept_id
+		FROM cte1 as t4
+		left join {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t5 on t5.source_code = t4.source_value
+		WHERE upper(t5.source_vocabulary_id) IN ( 'ICD10','HESAE_DIAG_STCM','READ')
+)
+INSERT INTO {CHUNK_SCHEMA}.stem_source_{CHUNK_ID} (
+    domain_id, person_id, visit_occurrence_id, visit_detail_id, provider_id, concept_id, source_value,
+    source_concept_id, type_concept_id, start_date, end_date, start_time, days_supply, dose_unit_concept_id,
+    dose_unit_source_value, effective_drug_dose, lot_number, modifier_source_value, 
+    operator_concept_id, qualifier_concept_id, qualifier_source_value, quantity, 
+    range_high, range_low, refills, route_concept_id, route_source_value, sig, stop_reason, unique_device_id, unit_concept_id,
+    unit_source_value, value_as_concept_id, value_as_number, value_as_string,
+    value_source_value, anatomic_site_concept_id, disease_status_concept_id, specimen_source_id, anatomic_site_source_value, disease_status_source_value, 
+    modifier_concept_id, stem_source_table, stem_source_id
+)
+SELECT 
+    NULL AS domain_id,
+    b.person_id, 
+    v.visit_occurrence_id,
+    vd.visit_detail_id,
+    b.provider_id,
+    NULL::INT AS concept_id,
+    b.source_value,
+    b.source_concept_id,
+    32818 AS type_concept_id,
+    b.start_date,
+    b.end_date,
+    '00:00:00'::TIME AS start_time,
+    NULL AS days_supply,
+    NULL::INT AS dose_unit_concept_id,
+    NULL AS dose_unit_source_value, 
+    NULL AS effective_drug_dose, 
+    NULL AS lot_number, 
+    NULL AS modifier_source_value,
+    NULL::INT AS operator_concept_id,
+    NULL::INT AS qualifier_concept_id,
+    NULL AS qualifier_source_value, 
+    NULL AS quantity, 
+    NULL::DOUBLE PRECISION AS range_high,
+    NULL::DOUBLE PRECISION AS range_low,
+    NULL AS refills,
+    NULL::INT AS route_concept_id,
+    NULL AS route_source_value,
+    NULL::INT AS unit_concept_id,
+    NULL AS sig, 
+    NULL AS stop_reason, 
+    NULL::INT AS unique_device_id,
+    NULL AS unit_source_value,
+    NULL::INT AS value_as_concept_id,
+    NULL AS value_as_number,
+    NULL AS value_as_string,
+    NULL AS value_source_value,
+    NULL::INT AS anatomic_site_concept_id,
+    b.disease_status_concept_id,
+    NULL::INT AS specimen_source_id,
+    NULL AS anatomic_site_source_value, 
+    b.disease_status_source_value, 
+    NULL::INT AS modifier_concept_id,
+    'hesae_diagnosis' AS stem_source_table,
+    NULL AS stem_source_id
+FROM cte2 AS b
+INNER JOIN {TARGET_SCHEMA}.visit_occurrence AS v ON v.person_id = b.person_id AND v.visit_source_value = b.visit_source_value
+INNER JOIN {TARGET_SCHEMA}.visit_detail AS vd ON b.person_id = vd.person_id AND b.visit_detail_source_value = vd.visit_detail_source_value
+WHERE vd.visit_detail_concept_id = 9201;
+
+
+create index idx_hesae_stem_source_{CHUNK_ID} on {CHUNK_SCHEMA}.stem_source_{CHUNK_ID} (source_concept_id) TABLESPACE pg_default;
+
+-----------------------------------------
+-- UPDATE CHUNK
+-----------------------------------------
+update {CHUNK_SCHEMA}.chunk 
+set stem_source_tbl = concat('stem_source_',{CHUNK_ID}::varchar)
+where chunk_id = {CHUNK_ID};
