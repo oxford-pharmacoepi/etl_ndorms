a962636c777b0774aef0e8c2db674c8819768a2e
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-02 16:26:13 +0100

    Rename death_patient to ons_death

--- sql_scripts/1d_ons_curation.sql
+++ sql_scripts/1d_ons_curation.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.death_patient CASCADE;
+DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.ons_death CASCADE;
 
-CREATE TABLE {SOURCE_NOK_SCHEMA}.death_patient (LIKE {SOURCE_SCHEMA}.death_patient) TABLESPACE pg_default;
+CREATE TABLE {SOURCE_NOK_SCHEMA}.ons_death (LIKE {SOURCE_SCHEMA}.ons_death) TABLESPACE pg_default;
 
 with cte1 as (
 	select *
-	from {SOURCE_SCHEMA}.death_patient
+	from {SOURCE_SCHEMA}.ons_death
 	where cause is null
 	or match_rank > 2
 )
-INSERT INTO {SOURCE_NOK_SCHEMA}.death_patient
-SELECT t1.* FROM {SOURCE_SCHEMA}.death_patient as t1
+INSERT INTO {SOURCE_NOK_SCHEMA}.ons_death
+SELECT t1.* FROM {SOURCE_SCHEMA}.ons_death as t1
 INNER JOIN cte1 on cte1.patid = t1.patid;
 
-alter table {SOURCE_NOK_SCHEMA}.death_patient add constraint pk_dpatient_nok primary key (patid) USING INDEX TABLESPACE pg_default;
+alter table {SOURCE_NOK_SCHEMA}.ons_death add constraint pk_dpatient_nok primary key (patid) USING INDEX TABLESPACE pg_default;
 
-DELETE FROM {SOURCE_SCHEMA}.death_patient as t1 
-using {SOURCE_NOK_SCHEMA}.death_patient as t2
+DELETE FROM {SOURCE_SCHEMA}.ons_death as t1 
+using {SOURCE_NOK_SCHEMA}.ons_death as t2
 WHERE t1.patid = t2.patid;
