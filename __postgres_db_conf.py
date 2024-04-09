03d24dead4aaae5e24202ceb73e2c4e4bc5061db
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-04-09 10:46:21 +0100

    Update __postgres_db_conf.py

--- __postgres_db_conf.py
+++ __postgres_db_conf.py
@@ -2,7 +2,7 @@ db_conf = dict({
 	'username': 'user',
 	'password': 'pwd',
 	'database': 'database_name',
-	'data_provider': 'name_of_provider',	#e.g. 'cprd'
+	'data_provider': 'name_of_provider',	#e.g. 'cprd', 'thin'
 	'database_type': 'type of database',	#e.g. 'gold, 'aurum', 'hesapc'
 	'source_release_date': 'date',			#e.g. '2022-05-01'
 	'cdm_version': '5.3',					#e.g. '5.3', '5.4'
@@ -12,24 +12,29 @@ db_conf = dict({
 	'source_nok_schema': 'source_nok',
 	'target_schema': 'public',
 	'vocabulary_schema': 'public',			#e.g. 'public', 'vocabulary'
+	'target_schema_to_link': 'schema to which we want to link the data',	#e.g. 'public_old'
+	'chunk_schema': 'chunk_hesop',
 	'max_workers' : 5,
 	'chunk_size': 1000,
-	'chunk_limit': 0,						#Do not change below here
+	'chunk_limit': 0,
 	'dir_processed': '/processed/',
-	'dir_log': '<project_directory>\\log',
-	'dir_stcm': '<project_directory>\\source_to_concept_map',
-	'dir_source_data': '<project_directory>\\data',
-	'dir_lookup': '<project_directory>\\lookups',
-	'dir_voc': '<project_directory>\\vocabulary',
+#	'dir_study': '<project_directory>/',
+	'dir_log': '<project_directory>/log',
+	'dir_stcm': '<project_directory>/source_to_concept_map',
+	'dir_source_data': '<project_directory>/data',
+	'dir_lookup': '<project_directory>/lookups',
+	'dir_voc': '<project_directory>/vocabulary', #Do not change below here
 	'tbl_gold': ['additional', 'clinical', 'consultation', 'immunisation', 'patient', 'practice', 'referral', 'staff', 'test', 'therapy'],
 	'tbl_gold_lookup':['batchnumber', 'bnfcodes', 'common_dosages', 'entity', 'medical', 'packtype', 'product', 'scoremethod', 'txtfiles\\*.txt'],
 	'tbl_aurum' : ['practice', 'staff', 'patient', 'consultation', 'observation', 'problem', 'referral', 'drugissue'],
 	'tbl_aurum_lookup': ['gender', 'region', 'jobcat', 'numunit', 'quantunit', 'refservicetype', 'medicaldictionary', 'productdictionary', 'visiontoemismigrators'],
 	'tbl_aurum_denom': ['aurum_acceptablepats', 'aurum_practices'],
+	'tbl_hes_lookup': ['linkage_coverage'],
+	'tbl_hesapc': ['hes_patient', 'hes_hospital', 'hes_acp', 'hes_ccare', 'hes_maternity', 'hes_episodes', 'hes_diagnosis_hosp', 'hes_primary_diag_hosp', 'hes_diagnosis_epi', 'hes_procedures_epi', 'hes_hrg'],
+	'tbl_hesop': ['hesop_patient', 'hesop_appointment', 'hesop_clinical'],
+	'tbl_hesae': ['hesae_patient', 'hesae_attendance', 'hesae_diagnosis', 'hesae_investigation', 'hesae_treatment', 'hesae_hrg', 'hesae_pathway'],
+	'tbl_ons': ['death_patient'],
 	'tbl_cprd': ['denominator', 'documentation', 'lookups', 'reference'],
 	'tbl_cdm': ['care_site', 'condition_era', 'condition_occurrence', 'death', 'device_exposure', 'dose_era', 'drug_era', 'drug_exposure', 'location', 'measurement', 'observation', 'observation_period', 'person', 'procedure_occurrence', 'provider', 'visit_detail', 'visit_occurrence'],
-	'tbl_cdm_voc': ['drug_strength', 'concept', 'concept_relationship', 'concept_ancestor', 'concept_synonym', 'vocabulary', 'relationship', 'concept_class', 'domain'],
-	'tbl_ons': ['death_patient'] 
+	'tbl_cdm_voc': ['drug_strength', 'concept', 'concept_relationship', 'concept_ancestor', 'concept_synonym', 'vocabulary', 'relationship', 'concept_class', 'domain']
 })
-
-
