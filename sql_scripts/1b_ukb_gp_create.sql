65d573a88e64bc0fd390b56c5bfaa1d6fba627f1
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-18 14:00:42 +0100

    Update 1b_ukb_gp_create.sql
    Updated as suggested.

--- sql_scripts/1b_ukb_gp_create.sql
+++ sql_scripts/1b_ukb_gp_create.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.gp_clinical (
 	eid					bigint,
 	data_provider		int,
 	event_dt			date,
-	read_2				varchar(50),
-	read_3				varchar(50),
+	read_2				varchar(7),
+	read_3				varchar(7),
 	value1				VARCHAR(20),
 	value2				VARCHAR(20),
 	value3				VARCHAR(20)
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.gp_scripts (
 	eid					bigint,	
 	data_provider		int,
 	issue_date			date,
-	read_2				varchar(50),
-	bnf_code			varchar(50),
+	read_2				varchar(7),
+	bnf_code			varchar(14),
 	dmd_code			varchar(20),
 	drug_name			varchar(500),
-	quantity			varchar(10)
+	quantity			varchar(20)
 )TABLESPACE pg_default;
