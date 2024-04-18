80419d456ffca7bb3df0d80b4260d972a01dfea1
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-18 15:37:28 +0100

    Updated table names as suggested.

--- sql_scripts/1b_biob_death_create.sql
+++ sql_scripts/1b_biob_death_create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death_row1 (
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death (
 	eid					bigint,	
 	ins_index			int,
 	dsource				VARCHAR(4),
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death_row1 (
 	date_of_death		date
 )TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death_cause_row1 (
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death_cause (
 	eid					bigint,	
 	ins_index			int,
 	arr_index			int,
