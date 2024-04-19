3cced15d9b791308973925e8f65605844480405c
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-19 11:33:34 +0100

    Added creation of PK idx for ukb_death tables.
    Added creation of PK idx for ukb_death tables.

---
+++ sql_scripts/1c_ukb_pk_idx_death_cause.sql
@@ -0,0 +1,2 @@
+alter table {SOURCE_SCHEMA}.death_cause add constraint pk_death_cause primary key (eid, ins_index, arr_index) USING INDEX TABLESPACE pg_default;
+create index idx_death_cause_level on {SOURCE_SCHEMA}.death_cause(level) TABLESPACE pg_default;
