b8c8c74b81bb07e90af6ea14d2fbce818ce0c155
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-22 17:39:51 +0100

    bug fixed in ons_curation
    removed duplicated records by using UNION

--- sql_scripts/1d_ons_curation.sql
+++ sql_scripts/1d_ons_curation.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.ons_death CASCADE;
+
 CREATE TABLE {SOURCE_NOK_SCHEMA}.ons_death (LIKE {SOURCE_SCHEMA}.ons_death) TABLESPACE pg_default;
 with cte1 as (
 	select *
@@ -7,20 +8,19 @@ with cte1 as (
 	or match_rank > 2
 )
 INSERT INTO {SOURCE_NOK_SCHEMA}.ons_death
-SELECT t1.* FROM {SOURCE_SCHEMA}.ons_death as t1
-INNER JOIN cte1 on cte1.patid = t1.patid;
-
-INSERT INTO {SOURCE_NOK_SCHEMA}.ons_death
+(SELECT t1.* FROM {SOURCE_SCHEMA}.ons_death as t1
+INNER JOIN cte1 on cte1.patid = t1.patid
+UNION
 select t1.*
 from {SOURCE_SCHEMA}.ons_death as t1
-where t2.data_source = 'ons_death';
-
 join source.linkage_coverage as t2 on t1.dod < t2.start or t1.dod > t2.end 
+where t2.data_source = 'ons_death'
+UNION
 -- Eliminate dead patients exist in source_nok.patient
-INSERT INTO {SOURCE_NOK_SCHEMA}.ons_death
 select t1.*
 from {SOURCE_SCHEMA}.ons_death as t1
 join source_nok.patient as t2 on t1.patid = t2.patid
+);
 
 alter table {SOURCE_NOK_SCHEMA}.ons_death add constraint pk_dpatient_nok primary key (patid) USING INDEX TABLESPACE pg_default;
 
