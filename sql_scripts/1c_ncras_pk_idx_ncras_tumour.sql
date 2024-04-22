7e322321f3ec19963f816cb90237d149c5e0c34c
Mandickel Kamtengeni <mandickel@gmail.com>
2024-04-22 10:11:51 +0100

    NCRAS PK creation Script Correction.

--- sql_scripts/1c_ncras_pk_idx_ncras_tumour.sql
+++ sql_scripts/1c_ncras_pk_idx_ncras_tumour.sql
@@ -1,3 +1,3 @@
 --ncras_tumour
-ALTER TABLE {SOURCE_SCHEMA}.ncras_tumour ADD CONSTRAINT pk_ncras_tumour PRIMARY KEY (e_patid,e_cr_patid) USING INDEX TABLESPACE pg_default;
+ALTER TABLE {SOURCE_SCHEMA}.ncras_tumour ADD CONSTRAINT pk_ncras_tumour PRIMARY KEY (e_patid,e_cr_patid,e_cr_id) USING INDEX TABLESPACE pg_default;
 create index idx_ncras_tumour_e_patid on {SOURCE_SCHEMA}.ncras_tumour (e_patid) TABLESPACE pg_default;
