f05d49bb693eeadd97c87941ba0f9614e7dcbd49
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-04-23 09:23:07 +0100

    Update 1d_ons_curation.sql
    replaced UNION by left join

--- sql_scripts/1d_ons_curation.sql
+++ sql_scripts/1d_ons_curation.sql
@@ -1,25 +1,15 @@
 DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.ons_death CASCADE;
 
 CREATE TABLE {SOURCE_NOK_SCHEMA}.ons_death (LIKE {SOURCE_SCHEMA}.ons_death) TABLESPACE pg_default;
-with cte1 as (
-	select *
-	from {SOURCE_SCHEMA}.ons_death
-	where match_rank > 2
-)
+
 INSERT INTO {SOURCE_NOK_SCHEMA}.ons_death
-(SELECT t1.* FROM {SOURCE_SCHEMA}.ons_death as t1
-INNER JOIN cte1 on cte1.patid = t1.patid
-UNION
 select t1.*
 from {SOURCE_SCHEMA}.ons_death as t1
-join source.linkage_coverage as t2 on t1.dod < t2.start or t1.dod > t2.end 
-where t2.data_source = 'ons_death'
-UNION
--- Eliminate dead patients exist in source_nok.patient
-select t1.*
-from {SOURCE_SCHEMA}.ons_death as t1
-join source_nok.patient as t2 on t1.patid = t2.patid
-);
+left join source.linkage_coverage as t2 on t2.data_source = 'ons_death' 
+left join source_nok.patient as t3 on t1.patid = t3.patid
+where t1.match_rank > 2 									-- Eliminate dead patients which match_rank > 2 	
+or t1.dod < t2.start or t1.dod > t2.end						-- Eliminate dead patients out of linkage_coverage period
+or t1.patid = t3.patid;										-- Eliminate dead patients exist in source_nok.patient
 
 alter table {SOURCE_NOK_SCHEMA}.ons_death add constraint pk_dpatient_nok primary key (patid) USING INDEX TABLESPACE pg_default;
 
