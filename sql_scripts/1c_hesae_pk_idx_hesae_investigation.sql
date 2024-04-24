5275c9822713e1c14cad18b496e074b1306a7190
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-04-24 13:38:16 +0100

    Corrected wrong indexing

--- sql_scripts/1c_hesae_pk_idx_hesae_investigation.sql
+++ sql_scripts/1c_hesae_pk_idx_hesae_investigation.sql
@@ -1,4 +1,4 @@
 --hesae_investigation
-ALTER TABLE {SOURCE_SCHEMA}.hesae_investigation ADD CONSTRAINT pk_hesae_investigation PRIMARY KEY (patid, aekey) USING INDEX TABLESPACE pg_default;
-create index idx_hesae_investigation_patid on {SOURCE_SCHEMA}.hesae_investigation(patid,aekey) TABLESPACE pg_default;
+ALTER TABLE {SOURCE_SCHEMA}.hesae_investigation ADD CONSTRAINT pk_hesae_investigation PRIMARY KEY (patid, aekey, invest_order) USING INDEX TABLESPACE pg_default;
+create index idx_hesae_investigation_patid on {SOURCE_SCHEMA}.hesae_investigation(patid,aekey, invest_order) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hesae_investigation using idx_hesae_investigation_patid;
--- sql_scripts/1c_hesae_pk_idx_hesae_treatment.sql
+++ sql_scripts/1c_hesae_pk_idx_hesae_treatment.sql
@@ -1,4 +1,4 @@
 --hesae_treatment
-ALTER TABLE {SOURCE_SCHEMA}.hesae_treatment ADD CONSTRAINT pk_hesae_treatment PRIMARY KEY (patid, aekey) USING INDEX TABLESPACE pg_default;
-create index idx_hesae_treatment_patid on {SOURCE_SCHEMA}.hesae_treatment(patid,aekey) TABLESPACE pg_default;
+ALTER TABLE {SOURCE_SCHEMA}.hesae_treatment ADD CONSTRAINT pk_hesae_treatment PRIMARY KEY (patid, aekey, treat_order) USING INDEX TABLESPACE pg_default;
+create index idx_hesae_treatment_patid on {SOURCE_SCHEMA}.hesae_treatment(patid, aekey, treat_order) TABLESPACE pg_default;
 cluster {SOURCE_SCHEMA}.hesae_treatment using idx_hesae_treatment_patid;
