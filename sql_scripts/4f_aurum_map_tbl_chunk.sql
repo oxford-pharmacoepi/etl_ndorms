470cf88753674a2cd8294522b82c39828a07fe72
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-04-29 16:35:22 +0100

    Update 4f_aurum_map_tbl_chunk.sql
    Added TABLESPACE

--- sql_scripts/4f_aurum_map_tbl_chunk.sql
+++ sql_scripts/4f_aurum_map_tbl_chunk.sql
@@ -1,37 +1,37 @@
-CREATE SCHEMA IF NOT EXISTS CHUNKS;
+CREATE SCHEMA IF NOT EXISTS {CHUNK_SCHEMA};
 --------------------------------
 -- CHUNK_PERSON
 --------------------------------
-DROP TABLE IF EXISTS chunks.chunk_person;
+DROP TABLE IF EXISTS {CHUNK_SCHEMA}.chunk_person;
 
-CREATE TABLE chunks.chunk_person AS 
+CREATE TABLE {CHUNK_SCHEMA}.chunk_person TABLESPACE pg_default AS 
 	select (floor((row_number() over (order by person_id)-1)/{CHUNK_SIZE}) + 1)::int as chunk_id,
 	person_id
 	FROM {TARGET_SCHEMA}.person
 	order by chunk_id, person_id;
 
-ALTER TABLE chunks.chunk_person ADD CONSTRAINT pk_chunk_person PRIMARY KEY (chunk_id, person_id);
-CREATE UNIQUE INDEX idx_chunk_person_id ON chunks.chunk_person (chunk_id, person_id ASC);
-CLUSTER chunks.chunk_person USING idx_chunk_person_id;
+ALTER TABLE {CHUNK_SCHEMA}.chunk_person ADD CONSTRAINT pk_chunk_person PRIMARY KEY (chunk_id, person_id) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX idx_chunk_person_id ON {CHUNK_SCHEMA}.chunk_person (chunk_id, person_id ASC) TABLESPACE pg_default ;
+CLUSTER {CHUNK_SCHEMA}.chunk_person USING idx_chunk_person_id;
 
 --------------------------------
 -- CHUNK
 --------------------------------
-DROP TABLE IF EXISTS chunks.chunk;
+DROP TABLE IF EXISTS {CHUNK_SCHEMA}.chunk;
 
-CREATE TABLE chunks.chunk AS 
+CREATE TABLE {CHUNK_SCHEMA}.chunk TABLESPACE pg_default AS 
 SELECT distinct chunk_id,
 		null::varchar(20) as stem_source_tbl,
 		null::varchar(20) as stem_tbl,
 		null::bigint as stem_id_start,
 		null::bigint as stem_id_end,
 		0::smallint as completed 
-FROM chunks.chunk_person;
+FROM {CHUNK_SCHEMA}.chunk_person;
 
-ALTER TABLE chunks.chunk ADD CONSTRAINT pk_chunk PRIMARY KEY (chunk_id);
-CREATE UNIQUE INDEX idx_chunk_id ON chunks.chunk (chunk_id ASC);
-CLUSTER chunks.chunk USING idx_chunk_id;
-CREATE INDEX idx_chunk_completed ON chunks.chunk (completed);
+ALTER TABLE {CHUNK_SCHEMA}.chunk ADD CONSTRAINT pk_chunk PRIMARY KEY (chunk_id) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX idx_chunk_id ON {CHUNK_SCHEMA}.chunk (chunk_id ASC) TABLESPACE pg_default ;
+CLUSTER {CHUNK_SCHEMA}.chunk USING idx_chunk_id;
+CREATE INDEX idx_chunk_completed ON {CHUNK_SCHEMA}.chunk (completed) TABLESPACE pg_default ;
 
 --------------------------------
 -- DROP TABLES CREATED BY CHUNKING
