fef418921c37e741f4e3e645c6483636773f800e
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-05-01 16:30:59 +0100

    Update 4d_ons_update_tbl_observation_period.sql
    bug fixed.

--- sql_scripts/4d_ons_update_tbl_observation_period.sql
+++ sql_scripts/4d_ons_update_tbl_observation_period.sql
@@ -1,8 +1,8 @@
 -- update observation_period from updated_death
 With temp1 AS(
 	select t1.observation_period_id, t2.death_date
-	from {TARGET_SCHEMA}.observation_period as t1
-	join {TARGET_SCHEMA}.death as t2 on t1.person_id = t2.person_id
+	from {TARGET_SCHEMA_TO_LINK}.observation_period as t1
+	join {TARGET_SCHEMA_TO_LINK}.death as t2 on t1.person_id = t2.person_id
 )
 update {TARGET_SCHEMA_TO_LINK}.observation_period as t1
 SET observation_period_end_date = t2.death_date
