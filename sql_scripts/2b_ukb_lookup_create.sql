bcd0d67decc9909fd125eec5b17cf1f7bd5d339f
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-07-25 14:39:22 +0100

    Update 2b_ukb_lookup_create.sql

--- sql_scripts/2b_ukb_lookup_create.sql
+++ sql_scripts/2b_ukb_lookup_create.sql
@@ -1,11 +1,23 @@
-CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}."269"
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.lookup269
 (
 	code smallint NOT NULL,
 	description varchar(50) NOT NULL
 )TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}."270"
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.lookup270
 (
 	code smallint NOT NULL,
 	description varchar(50) NOT NULL
+)TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.lookup626
+(
+	code smallint NOT NULL,
+	description varchar(20) NOT NULL
+)TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.lookup10
+(
+	code smallint NOT NULL,
+	description varchar(20) NOT NULL
 )TABLESPACE pg_default;
