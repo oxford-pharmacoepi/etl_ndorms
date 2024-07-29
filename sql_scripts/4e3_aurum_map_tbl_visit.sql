1075934a9f0bb29dc154e8837b83d3ddeaf1d244
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-07-29 11:01:05 +0100

    Added missing TARGET_SCHEMA

--- sql_scripts/4e3_aurum_map_tbl_visit.sql
+++ sql_scripts/4e3_aurum_map_tbl_visit.sql
@@ -34,8 +34,8 @@ FROM cte1;
 
 alter table {TARGET_SCHEMA}.visit_occurrence add constraint xpk_visit_occurrence primary key (visit_occurrence_id) USING INDEX TABLESPACE pg_default;;
 create index idx_visit_occ1 on {TARGET_SCHEMA}.visit_occurrence (person_id, visit_start_date, care_site_id) TABLESPACE pg_default;
-CLUSTER visit_occurrence USING idx_visit_occ1;
-CREATE INDEX idx_visit_concept_id ON visit_occurrence (visit_concept_id ASC) TABLESPACE pg_default;
+CLUSTER {TARGET_SCHEMA}.visit_occurrence USING idx_visit_occ1;
+CREATE INDEX idx_visit_concept_id ON {TARGET_SCHEMA}.visit_occurrence (visit_concept_id ASC) TABLESPACE pg_default;
 
 --------------------------------
 -- VISIT_DETAIL
