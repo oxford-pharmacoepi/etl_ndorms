22d3b3ee10abea9761ed1b9fe26a9c862fcd9425
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2024-09-23 16:19:06 +0100

    Update merge_db_linked.sql

--- sql_scripts/merge_db_linked.sql
+++ sql_scripts/merge_db_linked.sql
@@ -39,13 +39,15 @@ BEGIN
 -- 2. Insert content to all vocabularies in schema3 (copy the more recent from schema1 or schema2 as appropriate)
 ------------------------------------------------------
     FOR query1 IN 
-		SELECT format('INSERT INTO %I.%I SELECT * FROM %I.%I', schema3, tablename, schema1, tablename)
-		FROM pg_tables WHERE schemaname = schema1
+		SELECT format('INSERT INTO %I.%I SELECT * FROM %I.%I', schema3, tablename, schema2, tablename)
+		FROM pg_tables WHERE schemaname = schema2
 		AND tablename IN ('drug_strength', 'concept', 'concept_relationship', 'concept_ancestor', 'concept_synonym', 'vocabulary', 'relationship', 'concept_class', 'domain')
     LOOP
 		RAISE NOTICE 'appliying %', query1;
 		EXECUTE query1;
     END LOOP;
+------------------------------------------------------
+-- 2.B Add CONSTRAINTS for VOCABULARY AND recreate source_to_concept_map + related tables (py 3_load_cdm_vocabulary.py -FD:\cprd\...)
 ------------------------------------------------------
 -- 3. Insert content to schema3 from primary source (schema1) of all CDM tables non-patient related
 ------------------------------------------------------
@@ -61,7 +63,8 @@ BEGIN
 -- 4. Insert content to schema3 from primary source (schema1) of all CDM tables patient related and exclude those records belonging to `tbl_to_delete`
 ------------------------------------------------------
     FOR query1 IN 
-		SELECT format('INSERT INTO %I.%I SELECT t1.* FROM %I.%I AS t1
+		SELECT format('INSERT INTO %I.%I 
+			SELECT t1.* FROM %I.%I AS t1
 			LEFT JOIN %I.%I AS t2 on t1.person_id = t2.patid
 			WHERE t2.patid is null;', schema3, tablename, schema1, tablename, schema1, tbl_to_delete)
 		FROM pg_tables WHERE schemaname = schema1
@@ -71,20 +74,27 @@ BEGIN
 		EXECUTE query1;
     END LOOP;
 ------------------------------------------------------
--- 5. Update schema3.PERSON table with race information taken from the linked source (source2) if not already present
+-- 5. Add INDEX to PERSON
 ------------------------------------------------------
-	query1 = format('UPDATE %I.%I AS t1
-					SET race_concept_id = t2.race_concept_id,
-					race_source_value = t2.race_source_value
-					FROM %I.%I AS t2 
-					WHERE t1.person_id = t2.person_id
-					AND t1.race_concept_id is null;', schema3, 'person', schema2, 'person');
+	query1 = format('CREATE UNIQUE INDEX IF NOT EXISTS idx_person_id ON %I.person (person_id ASC) TABLESPACE pg_default;',schema3);
 	EXECUTE query1;
 ------------------------------------------------------
--- 6. Insert content from linked source (schema2) of all CDM tables patient related (escluding DEATH, OBSERVATION_PERIOD, PERSON) and exclude those records belonging to `tbl_to_delete`
+-- 6. Update schema3.PERSON table with race information taken from the linked source (source2) if not already present
+------------------------------------------------------
+	query1 = format('UPDATE %I.person AS t1
+					SET race_concept_id = t2.race_concept_id,
+					race_source_value = t2.race_source_value
+					FROM %I.person AS t2 
+					WHERE t1.person_id = t2.person_id
+					AND t1.race_concept_id is null;', schema3, schema2);
+	RAISE NOTICE 'appliying %', query1;
+	EXECUTE query1;
+------------------------------------------------------
+-- 7. Insert content from linked source (schema2) of all CDM tables patient related (escluding DEATH, OBSERVATION_PERIOD, PERSON) and exclude those records belonging to `tbl_to_delete`
 ------------------------------------------------------
     FOR query1 IN 
-		SELECT format('INSERT INTO %I.%I SELECT t1.* FROM %I.%I AS t1
+		SELECT format('INSERT INTO %I.%I 
+			SELECT t1.* FROM %I.%I AS t1
 			LEFT JOIN %I.%I AS t2 on t1.person_id = t2.patid
 			WHERE t2.patid is null;', schema3, tablename, schema2, tablename, schema1, tbl_to_delete)
 		FROM pg_tables WHERE schemaname = schema1
@@ -94,17 +104,13 @@ BEGIN
 		EXECUTE query1;
     END LOOP;
 ------------------------------------------------------
--- 7. Insert content to source3.PROVIDER from source2.PROVIDER
+-- 8. Insert content to source3.PROVIDER from source2.PROVIDER
 ------------------------------------------------------
-   FOR query1 IN 
-		SELECT format('INSERT INTO %I.provider SELECT * FROM %I.provider;', schema3, schema2)
-		FROM pg_tables WHERE schemaname = schema1
-    LOOP
-		RAISE NOTICE 'appliying %', query1;
-		EXECUTE query1;
-    END LOOP;
+	query1 = format('INSERT INTO %I.provider SELECT * FROM %I.provider;', schema3, schema2);
+	RAISE NOTICE 'appliying %', query1;
+	EXECUTE query1;
 ------------------------------------------------------
--- 8. Update source3.OBSERVATION_PERIOD to consider all merged data sources (schema1, schema2)
+-- 9. Update source3.OBSERVATION_PERIOD to consider all merged data sources (schema1, schema2)
 -- We leave the OBSERVATION_PERIOD records from Primary Care (GOLD/AURUM) unchanged (period_type_concept_id=32880)
 -- We insert/update an OBSERVATION_PERIOD record per patient with the "summary" of all observation_periods (period_type_concept_id=32882)
 ------------------------------------------------------
@@ -143,12 +149,11 @@ BEGIN
 			EXECUTE query1;
 	END IF;
 ------------------------------------------------------
--- 9. Insert content to source3._PATID_DELETED from source1._PATID_DELETED (always present)
+-- 10. Insert content to source3._PATID_DELETED from source1._PATID_DELETED (always present)
 ------------------------------------------------------
 	query1 = format('INSERT INTO %I._patid_deleted SELECT * FROM %I._patid_deleted;', schema3, schema1);
 	RAISE NOTICE 'appliying %', query1;
 	EXECUTE query1;
--- 10. Add CONSTRAINTS for VOCABULARY AND recreate source_to_concept_map + related tables (py 3_load_cdm_vocabulary.py -FD:\cprd\...)
 -- 11. Add CONSTRAINTS for CDM (py 5_build_cdm_pk_idx_fk.py -FD:\cprd\...)
 -- 12. Create table _records (py 7_count_cdm_records.py -FD:\cprd\...)
 -- 13. Check Tablespaces
