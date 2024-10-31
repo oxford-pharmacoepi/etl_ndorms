51bef5aecb3817c86402ac345c3b37a767bb4992
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-10-31 15:01:41 +0000

    removed death_cause in ukb_gp

--- sql_scripts/1b_ukb_gp_create.sql
+++ sql_scripts/1b_ukb_gp_create.sql
@@ -32,14 +32,6 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death (
 	date_of_death		date
 )TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death_cause (
-	eid					bigint,	
-	ins_index			int,
-	arr_index			int,
-	level				int,
-	cause_icd10			VARCHAR(8)
-)TABLESPACE pg_default;
-
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.gp_registrations (
 	eid					bigint,	
 	data_provider		int,
