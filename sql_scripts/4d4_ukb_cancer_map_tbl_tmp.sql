32af1c4d5d4821be7deb6e06b4fe0db5cb298f42
teenwyman <waiyi.man@ndorms.ox.ac.uk>
2024-12-09 09:46:13 +0000

    added max_id

--- sql_scripts/4d4_ukb_cancer_map_tbl_tmp.sql
+++ sql_scripts/4d4_ukb_cancer_map_tbl_tmp.sql
@@ -11,7 +11,16 @@ CREATE TABLE {SOURCE_SCHEMA}.temp_visit_detail
 	visit_source_value varchar(60)
 );
 
-With cte AS(
+WITH cte as (
+    SELECT max_id as start_id from {TARGET_SCHEMA_TO_LINK}._max_ids 
+	WHERE lower(tbl_name) = 'max_of_all'
+), cte0 as(
+	SELECT cte.start_id
+	FROM cte
+	UNION ALL
+	SELECT 0 as start_id
+	WHERE NOT EXISTS (SELECT 1 FROM cte)
+), cte1 AS(
 	select distinct 
 		t1.eid, 
 		t1.p40005 as diagnosis_date,
@@ -21,11 +30,11 @@ With cte AS(
 )
 insert into {SOURCE_SCHEMA}.temp_visit_detail
 select 
-	ROW_NUMBER () OVER ( ORDER BY eid, diagnosis_date, data_source) as visit_detail_id,
-	eid,
-	diagnosis_date,
-	data_source
-from cte;
+	ROW_NUMBER () OVER ( ORDER BY eid, diagnosis_date, data_source) + cte0.start_id as visit_detail_id,
+	cte1.eid,
+	cte1.diagnosis_date,
+	cte1.data_source
+from cte0, cte1;
 
 alter table {SOURCE_SCHEMA}.temp_visit_detail add constraint pk_temp_visit_d primary key (visit_detail_id);
 create index idx_temp_visit_1 on {SOURCE_SCHEMA}.temp_visit_detail (person_id, visit_detail_start_date, visit_source_value); 
