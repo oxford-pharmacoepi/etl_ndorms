1df9decbec553c1ca8712ba11030bda1cefca937
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2025-04-28 22:28:18 +0100

    Update 4i4_hesae_map_tbl_cdm.sql

--- sql_scripts/4i4_hesae_map_tbl_cdm.sql
+++ sql_scripts/4i4_hesae_map_tbl_cdm.sql
@@ -164,7 +164,7 @@ cte5 as (
 )
 
 insert into {TARGET_SCHEMA}.procedure_occurrence(procedure_occurrence_id, person_id, procedure_concept_id, procedure_date,
-												  procedure_datetime, procedure_end_ date, procedure_end_datetime, 
+												  procedure_datetime, procedure_end_date, procedure_end_datetime, 
 												  procedure_type_concept_id, modifier_concept_id, quantity,
 												  provider_id, visit_occurrence_id, visit_detail_id, procedure_source_value,
 												  procedure_source_concept_id, modifier_source_value)
