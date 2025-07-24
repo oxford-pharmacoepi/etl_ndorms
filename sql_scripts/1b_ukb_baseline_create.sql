80a07dbb9268f13233ae8b83fbb5b7b58a4df4d0
Mandickel Kamtengeni <mandickel@gmail.com>
2025-07-24 12:06:10 +0100

    Adding comments in 1b_ukb_baseline_create

--- sql_scripts/1b_ukb_baseline_create.sql
+++ sql_scripts/1b_ukb_baseline_create.sql
@@ -1,27 +1,27 @@
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.baseline (
-	eid				bigint 		not null,	
-	p31				smallint,
-	p34				smallint,
-	p52				smallint,
-	p53_i0			date,
-	p53_i1			date,
-	p53_i2			date,
-	p53_i3			date,
-	p54_i0			NUMERIC,
-	p54_i1			NUMERIC,
-	p54_i2			NUMERIC,
-	p54_i3			NUMERIC,
-	p200			date,
-	p20143			date,
-	p21000_i0		NUMERIC,
-	p21000_i1		NUMERIC,
-	p21000_i2		NUMERIC,
-	p21000_i3		NUMERIC,
-	p21022			int,
-	p22189			NUMERIC,
-	p26410			NUMERIC,
-	p26426			NUMERIC,
-	p26427			NUMERIC
+	eid				bigint 		not null, --patientID	
+	p31				smallint, --Sex
+	p34				smallint, --Year of birth
+	p52				smallint, --Month of birth
+	p53_i0			date, --Date of attending assessment centre
+	p53_i1			date, --Date of attending assessment centre
+	p53_i2			date, --Date of attending assessment centre
+	p53_i3			date, --Date of attending assessment centre
+	p54_i0			NUMERIC, --UK Biobank assessment centre
+	p54_i1			NUMERIC, --UK Biobank assessment centre
+	p54_i2			NUMERIC, --UK Biobank assessment centre
+	p54_i3			NUMERIC, --UK Biobank assessment centre
+	p200			date, --Date of consenting to join UK Biobank	
+	p20143			date, --Date of last personal contact with UK Biobank
+	p21000_i0		NUMERIC, --Ethnic background
+	p21000_i1		NUMERIC, --Ethnic background
+	p21000_i2		NUMERIC, --Ethnic background
+	p21000_i3		NUMERIC, --Ethnic background
+	p21022			int, --Age at recruitment
+	p22189			NUMERIC, --Townsend deprivation index at recruitment
+	p26410			NUMERIC, --Index of Multiple Deprivation (England)
+	p26426			NUMERIC, --	Index of Multiple Deprivation (Wales)
+	p26427			NUMERIC --	Index of Multiple Deprivation (Scotland)
 )TABLESPACE pg_default;
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.death (
