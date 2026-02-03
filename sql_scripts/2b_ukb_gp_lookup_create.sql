7f90a3e2caeeaa288cb4e3f33d6decec78df6415
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-02-03 14:43:20 +0000

    Update 2b_ukb_gp_lookup_create.sql
    gold_product structure changed

--- sql_scripts/2b_ukb_gp_lookup_create.sql
+++ sql_scripts/2b_ukb_gp_lookup_create.sql
@@ -6,17 +6,19 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.lookup626
 
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.gold_product
 (
-    prodcode bigint NOT NULL,
-    dmdcode character varying(20),
-    gemscriptcode character varying(8),
-    productname character varying(500),
-    drugsubstance character varying(1500),
-    strength character varying(1100),
-    formulation character varying(100),
-    route character varying(100),
-    bnfcode character varying(100),
-    bnfchapter character varying(500)
-	
+	prodcode bigint NOT NULL,
+	dmdcode varchar(20) DEFAULT NULL,
+	gemscriptcode varchar(8) DEFAULT NULL,
+	therapyevents bigint DEFAULT NULL,
+	productname varchar(500) DEFAULT NULL,
+	drugsubstance varchar(1500) DEFAULT NULL,
+	strength varchar(1100) DEFAULT NULL,
+	formulation varchar(100) DEFAULT NULL,
+	route varchar(110) DEFAULT NULL,
+	bnfcode varchar(100) DEFAULT NULL,
+	bnfchapter varchar(500) DEFAULT NULL,
+	databaserelease varchar(15) DEFAULT NULL
+
 )TABLESPACE pg_default;
 
 
