c66f7a0a077635962a009726a41061aab5b67121
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-02-05 17:18:18 +0000

    Removed, not necessary

--- sql_scripts/1c_ukb_hesin_pk_idx_baseline.sql
+++
@@ -1,2 +0,0 @@
---baseline
-alter table {SOURCE_SCHEMA}.baseline add constraint pk_baseline primary key (eid) USING INDEX TABLESPACE pg_default;
--- sql_scripts/1c_ukb_hesin_pk_idx_death.sql
+++
@@ -1,2 +0,0 @@
---death 
-alter table {SOURCE_SCHEMA}.death add constraint pk_death primary key (eid, ins_index) USING INDEX TABLESPACE pg_default;
