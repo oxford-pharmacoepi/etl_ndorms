00faaf484572e7925d87a6d1ce9680f2f795ff54
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-02-05 17:19:52 +0000

    Update 1d_ukb_gp_curation.sql
    Removed commented code

--- sql_scripts/1d_ukb_gp_curation.sql
+++ sql_scripts/1d_ukb_gp_curation.sql
@@ -2,15 +2,9 @@ DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.gp_clinical CASCADE;
 DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.gp_scripts CASCADE;
 DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.gp_registrations CASCADE;
 
---DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.baseline CASCADE;
---DROP TABLE IF EXISTS {SOURCE_NOK_SCHEMA}.death CASCADE;
-
 CREATE TABLE {SOURCE_NOK_SCHEMA}.gp_clinical (LIKE {SOURCE_SCHEMA}.gp_clinical) TABLESPACE pg_default;
 CREATE TABLE {SOURCE_NOK_SCHEMA}.gp_scripts (LIKE {SOURCE_SCHEMA}.gp_scripts) TABLESPACE pg_default;
 CREATE TABLE {SOURCE_NOK_SCHEMA}.gp_registrations (LIKE {SOURCE_SCHEMA}.gp_registrations) TABLESPACE pg_default;
-
---CREATE TABLE {SOURCE_NOK_SCHEMA}.baseline (LIKE {SOURCE_SCHEMA}.baseline) TABLESPACE pg_default;
---CREATE TABLE {SOURCE_NOK_SCHEMA}.death (LIKE {SOURCE_SCHEMA}.death) TABLESPACE pg_default;
 
 --------------------------------
 -- gp_clinical
@@ -18,7 +12,6 @@ CREATE TABLE {SOURCE_NOK_SCHEMA}.gp_registrations (LIKE {SOURCE_SCHEMA}.gp_regis
 INSERT INTO {SOURCE_NOK_SCHEMA}.gp_clinical
 (
 	select t1.* from {SOURCE_SCHEMA}.gp_clinical as t1
---	join {TARGET_SCHEMA_TO_LINK}._patid_deleted as t2 on t1.eid = t2.patid	--Remove withdrawal patients
 	left join {TARGET_SCHEMA_TO_LINK}.person as t2 on t1.eid = t2.person_id	--Remove withdrawal patients
 	where t2.person_id is null or t1.event_dt is null
 	
@@ -52,7 +45,6 @@ WHERE t1.id = t2.id;
 INSERT INTO {SOURCE_NOK_SCHEMA}.gp_scripts
 (
 	select t1.* from {SOURCE_SCHEMA}.gp_scripts as t1
---	join {TARGET_SCHEMA_TO_LINK}._patid_deleted as t2 on t1.eid = t2.patid	--Remove withdrawal patients
 	left join {TARGET_SCHEMA_TO_LINK}.person as t2 on t1.eid = t2.person_id	--Remove withdrawal patients
 	where t2.person_id is null or t1.issue_date is null
 
@@ -83,7 +75,6 @@ WHERE t1.id = t2.id;
 --Remove withdrawal patients
 insert into {SOURCE_NOK_SCHEMA}.gp_registrations
 select t1.* from {SOURCE_SCHEMA}.gp_registrations as t1
---join {TARGET_SCHEMA_TO_LINK}._patid_deleted as t2 on t1.eid = t2.patid
 left join {TARGET_SCHEMA_TO_LINK}.person as t2 on t1.eid = t2.person_id	--Remove withdrawal patients
 where t2.person_id is null;
 
@@ -101,44 +92,3 @@ where reg_date in ('1901-01-01', '1902-02-02', '1903-03-03', '1909-09-09', '2037
 
 DELETE FROM {SOURCE_SCHEMA}.gp_registrations  
 where reg_date in ('1901-01-01', '1902-02-02', '1903-03-03', '1909-09-09', '2037-07-07');
-
---------------------------------
--- baseline
---------------------------------
---With cte AS(
---	select distinct eid 
---	from {SOURCE_SCHEMA}.gp_registrations 
---	union 
---	select distinct eid 
---	from {SOURCE_SCHEMA}.gp_clinical 
---	union 
---	select distinct eid 
---	from {SOURCE_SCHEMA}.gp_scripts 
---)
---INSERT INTO {SOURCE_NOK_SCHEMA}.baseline
---select t1.*
---from {SOURCE_SCHEMA}.baseline as t1
---left join cte as t2 on t1.eid = t2.eid
---where t2.eid is null;
---
---alter table {SOURCE_NOK_SCHEMA}.baseline add constraint pk_baseline_nok primary key (eid) USING INDEX TABLESPACE pg_default;
---
---DELETE FROM {SOURCE_SCHEMA}.baseline as t1 
---using {SOURCE_NOK_SCHEMA}.baseline as t2
---WHERE t1.eid = t2.eid;
-
-
---------------------------------
--- death
---------------------------------
---INSERT INTO {SOURCE_NOK_SCHEMA}.death
---select t1.*
---from {SOURCE_SCHEMA}.death as t1
---join {SOURCE_NOK_SCHEMA}.baseline as t2 on t1.eid = t2.eid;		--Eliminate death with no gp records
---
---alter table {SOURCE_NOK_SCHEMA}.death add constraint pk_death primary key (eid, ins_index) USING INDEX TABLESPACE pg_default;
---
---DELETE FROM {SOURCE_SCHEMA}.death as t1 
---using {SOURCE_NOK_SCHEMA}.death as t2
---WHERE t1.eid = t2.eid
---and t1.ins_index = t2.ins_index;
