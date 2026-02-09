59b4cfed155756c8dba6078331054763ac65ab39
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-02-09 16:49:46 +0000

    Update 4e4_ukb_hesin_map_tbl_visit.sql
    Made it faster

--- sql_scripts/4e4_ukb_hesin_map_tbl_visit.sql
+++ sql_scripts/4e4_ukb_hesin_map_tbl_visit.sql
@@ -42,16 +42,38 @@ CREATE INDEX idx_visit_source_value ON {TARGET_SCHEMA}.visit_occurrence (visit_s
 drop table if exists {TARGET_SCHEMA}.visit_detail CASCADE;
 
 with cte1 AS (
-	select eid as person_id, spell_index::varchar(50) as visit_source_value, ins_index::varchar(50) as visit_detail_source_value,
-	CASE WHEN tretspef <> '&' THEN tretspef ELSE CASE WHEN mainspef <> '&' THEN mainspef ELSE Null END END as specialty
-	FROM {SOURCE_SCHEMA}.hesin
-),	
+	select eid as person_id, 
+	spell_index::varchar(50) as visit_source_value, 
+	ins_index::varchar(50) as visit_detail_source_value,
+	CASE WHEN tretspef <> '&' THEN tretspef ELSE CASE WHEN mainspef <> '&' THEN mainspef ELSE Null END END as provider_specialty,
+	t2.target_concept_id AS admitted_from_concept_id,
+	t2.source_code_description::varchar(100) AS admitted_from_source_value,
+	t3.source_code_description::varchar(100) AS discharged_to_source_value,
+	t3.target_concept_id::int AS discharged_to_concept_id
+	FROM {SOURCE_SCHEMA}.hesin as t1
+	LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t2 on t2.source_code = CONCAT('265-',t1.admisorc_uni) and t2.target_domain_id = 'Visit' and t2.source_vocabulary_id = 'UKB_ADMISORC_STCM'
+	LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t3 on t3.source_code = CONCAT('267-',t1.disdest_uni) and t3.target_domain_id = 'Visit' and t3.source_vocabulary_id = 'UKB_DISDEST_STCM'
+),
 cte2 AS (	
 	select t1.*, t3.provider_id
 	FROM cte1 as t1
-	LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t2 on t1.specialty = t2.source_code 
+	INNER JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t2 on t1.provider_specialty = t2.source_code 
+	and t2.source_vocabulary_id = 'HES Specialty'
+	INNER JOIN {TARGET_SCHEMA}.provider as t3 on t3.specialty_source_value = t2.source_code_description
+),
+cte3 AS (	
+	select t1.*, t3.provider_id
+	FROM cte1 as t1
+	LEFT JOIN cte2 as t4 on t1.person_id = t4.person_id and t1.visit_detail_source_value = t4.visit_detail_source_value and t1.provider_specialty = t4.provider_specialty
+	LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t2 on t1.provider_specialty = t2.source_code 
 	and t2.source_vocabulary_id = 'HES_SPEC_STCM'
 	LEFT JOIN {TARGET_SCHEMA}.provider as t3 on t3.specialty_source_value = t2.source_code_description
+	WHERE t4.provider_specialty is null
+),
+cte4 AS (
+	select * from cte2
+	UNION DISTINCT
+	select * from cte3
 )
 SELECT distinct 
 	t1.visit_detail_id,
@@ -66,20 +88,17 @@ SELECT distinct
 	NULL::int AS care_site_id,
 	t1.visit_detail_source_value as visit_detail_source_value,
 	NULL::int AS visit_detail_source_concept_id,
-	t4.target_concept_id AS admitted_from_concept_id,
-	t4.source_code_description::varchar(100) AS admitted_from_source_value,
-	t5.source_code_description::varchar(100) AS discharged_to_source_value,
-	t5.target_concept_id::int AS discharged_to_concept_id,
-	t6.visit_detail_id as preceding_visit_detail_id,
+	t2.admitted_from_concept_id,
+	t2.admitted_from_source_value,
+	t2.discharged_to_source_value,
+	t2.discharged_to_concept_id,
+	t3.visit_detail_id as preceding_visit_detail_id,
 	NULL::int AS parent_visit_detail_id,
 	t1.visit_occurrence_id
 into {TARGET_SCHEMA}.visit_detail
 FROM {SOURCE_SCHEMA}.temp_visit_detail as t1
-INNER JOIN cte2 as t2 on t1.person_id = t2.person_id and t1.visit_source_value = t2.visit_source_value and t1.visit_detail_source_value = t2.visit_detail_source_value
-INNER JOIN {SOURCE_SCHEMA}.hesin AS t3 ON t3.eid = t1.person_id AND t1.visit_source_value = t3.spell_index::varchar(50) and t1.visit_detail_source_value = t3.ins_index::varchar(50)
-LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t4 on t4.source_code = CONCAT('265-',t3.admisorc_uni) and t4.target_domain_id = 'Visit' and t4.source_vocabulary_id = 'UKB_ADMISORC_STCM'
-LEFT JOIN {VOCABULARY_SCHEMA}.source_to_standard_vocab_map as t5 on t5.source_code = CONCAT('267-',t3.disdest_uni) and t5.target_domain_id = 'Visit' and t5.source_vocabulary_id = 'UKB_DISDEST_STCM'
-LEFT JOIN {SOURCE_SCHEMA}.temp_visit_detail as t6 on t1.person_id = t6.person_id and (t1.visit_detail_id - 1) = t6.visit_detail_id;
+LEFT JOIN cte4 as t2 on t1.person_id = t2.person_id and t1.visit_source_value = t2.visit_source_value and t1.visit_detail_source_value = t2.visit_detail_source_value
+LEFT JOIN {SOURCE_SCHEMA}.temp_visit_detail as t3 on t1.person_id = t3.person_id and (t1.visit_detail_id - 1) = t3.visit_detail_id;
 
 
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT xpk_visit_detail PRIMARY KEY (visit_detail_id) USING INDEX TABLESPACE pg_default;	
