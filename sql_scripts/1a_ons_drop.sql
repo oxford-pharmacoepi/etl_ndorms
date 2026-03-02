d428cba3774cd4f2047ee1c467d6c428dd186791
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-03-02 11:56:28 +0000

    Update 1a_ons_drop.sql
    Removed  "linkage_coverage" from ONS schema as it is now in the "linkage" schema

--- sql_scripts/1a_ons_drop.sql
+++ sql_scripts/1a_ons_drop.sql
@@ -1,3 +1,3 @@
-drop table if exists {SOURCE_SCHEMA}.linkage_coverage CASCADE;
+--drop table if exists {SOURCE_SCHEMA}.linkage_coverage CASCADE;
 drop table if exists {SOURCE_SCHEMA}.ons_death CASCADE;
 drop table if exists {SOURCE_NOK_SCHEMA}.ons_death CASCADE;
