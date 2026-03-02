493021399acf09076ed800c9f6f3dd3510f25754
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-03-02 12:01:08 +0000

    Update 1c_ons_pk_idx_death_patient.sql
    Removed  "linkage_coverage" from ONS schema as it is now in the "linkage" schema
    Removed "match_rank" as it is not present anymore
    Changed "cause" to "s_cod_code_1" as it changed name

--- sql_scripts/1c_ons_pk_idx_death_patient.sql
+++ sql_scripts/1c_ons_pk_idx_death_patient.sql
@@ -1,5 +1,2 @@
-alter table {SOURCE_SCHEMA}.linkage_coverage add constraint pk_linkage_coverage primary key (data_source) USING INDEX TABLESPACE pg_default;
-
 alter table {SOURCE_SCHEMA}.ons_death add constraint pk_ons_death primary key (patid) USING INDEX TABLESPACE pg_default;
-create index idx_dpatient_match_rank on {SOURCE_SCHEMA}.ons_death(match_rank) TABLESPACE pg_default;
-create index idx_dpatient_cause on {SOURCE_SCHEMA}.ons_death(cause) TABLESPACE pg_default;
+create index idx_dpatient_cause on {SOURCE_SCHEMA}.ons_death(s_cod_code_1) TABLESPACE pg_default;
