c3e730c0a889a2c115ba018057149e4b24a58246
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-05-18 15:36:48 +0100

    e_patid changed to patid
    and e_cr_patid changed to cr_patid

--- sql_scripts/1c_ncrascr_pk_idx_treatment.sql
+++ sql_scripts/1c_ncrascr_pk_idx_treatment.sql
@@ -1,3 +1,3 @@
 --treatment
 ALTER TABLE {SOURCE_SCHEMA}.treatment ADD CONSTRAINT pk_treatment PRIMARY KEY (treatment_id) USING INDEX TABLESPACE pg_default;
-create index idx_treatment_e_patid on {SOURCE_SCHEMA}.treatment(e_patid) TABLESPACE pg_default;
+create index idx_treatment_patid on {SOURCE_SCHEMA}.treatment(patid) TABLESPACE pg_default;
--- sql_scripts/1d_ncrascr_curation.sql
+++ sql_scripts/1d_ncrascr_curation.sql
@@ -8,8 +8,8 @@ CREATE TABLE {SOURCE_NOK_SCHEMA}.treatment (LIKE {SOURCE_SCHEMA}.treatment) TABL
 INSERT INTO {SOURCE_NOK_SCHEMA}.tumour(
 	select t1.* 
 	from {SOURCE_SCHEMA}.tumour as t1
-	left join {SOURCE_NOK_SCHEMA_TO_LINK}.person as t2 on t1.e_patid = t2.person_id
-	where t2.person_id is null													-- Eliminated by patients not exist in {target_to_link}.person
+	left join {TARGET_SCHEMA_TO_LINK}.person as t2 on t1.patid = t2.person_id
+	where t2.person_id is null													-- Eliminate patients that do not exist in {TARGET_SCHEMA_TO_LINK}.person
 
 	UNION
 
@@ -19,20 +19,20 @@ INSERT INTO {SOURCE_NOK_SCHEMA}.tumour(
 	where t1.diagnosisdatebest < t2.start or t1.diagnosisdatebest > t2.end		-- Eliminate tumour out of linkage_coverage period
 );
 
-alter table {SOURCE_NOK_SCHEMA}.tumour add constraint pk_tumour_nok primary key (e_patid,e_cr_patid,e_cr_id) USING INDEX TABLESPACE pg_default;
+alter table {SOURCE_NOK_SCHEMA}.tumour add constraint pk_tumour_nok primary key (patid,cr_patid,cr_id) USING INDEX TABLESPACE pg_default;
 
 DELETE FROM {SOURCE_SCHEMA}.tumour as t1 
 using {SOURCE_NOK_SCHEMA}.tumour as t2
-WHERE t1.e_patid = t2.e_patid
-and t1.e_cr_patid = t2.e_cr_patid
-and t1.e_cr_id = t2.e_cr_id;
+WHERE t1.patid = t2.patid
+and t1.cr_patid = t2.cr_patid
+and t1.cr_id = t2.cr_id;
 
 -- treatment
 With cte as(																
 	select			
-		e_patid,					
-		e_cr_id,					
-		e_cr_patid,				
+		patid,					
+		cr_id,					
+		cr_patid,				
 		number_of_tumours,		
 		eventcode,				
 		eventdesc,				
@@ -42,23 +42,26 @@ With cte as(
 		opcs4_code,				
 		opcs4_name,				
 		radiocode,				
-		radiodesc,				
+		radiodesc,	
+		imagingcode,
+		imagingdesc,
+		imagingsite,
 		lesionsize,				
 		chemo_all_drug,			
 		chemo_drug_group, 
 		min(treatment_id) as min_treatment_id
 	from {SOURCE_SCHEMA}.treatment
-	group by 	e_patid, e_cr_id, e_cr_patid, number_of_tumours,		
+	group by 	patid, cr_id, cr_patid, number_of_tumours,		
 				eventcode, eventdesc, eventdate,				
 				within_six_months_flag,	six_months_after_flag, 
 				opcs4_code, opcs4_name, 
-				radiocode, radiodesc, lesionsize, 
+				radiocode, radiodesc, imagingcode, imagingdesc, imagingsite, lesionsize, 
 				chemo_all_drug, chemo_drug_group
 )
 INSERT INTO {SOURCE_NOK_SCHEMA}.treatment(
 	select t1.* 
 	from {SOURCE_SCHEMA}.treatment as t1
-	left join {SOURCE_NOK_SCHEMA_TO_LINK}.person as t2 on t1.e_patid = t2.person_id
+	left join {TARGET_SCHEMA_TO_LINK}.person as t2 on t1.patid = t2.person_id
 	where t2.person_id is null													-- Eliminated by patients not exist in {target_to_link}.person
 
 	UNION
