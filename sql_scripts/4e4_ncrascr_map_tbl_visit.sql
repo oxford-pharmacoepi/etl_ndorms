69c5fe619be64d31f99810490b9d20594de64d23
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-06-03 12:46:15 +0100

    Update 4e4_ncrascr_map_tbl_visit.sql

--- sql_scripts/4e4_ncrascr_map_tbl_visit.sql
+++ sql_scripts/4e4_ncrascr_map_tbl_visit.sql
@@ -7,7 +7,7 @@ with cte1 as (
 	SELECT distinct visit_occurrence_id, person_id, visit_detail_start_date
 	from {SOURCE_SCHEMA}.temp_visit_detail as t1
 )
-SELECT 
+SELECT distinct
 	t1.visit_occurrence_id,
 	t1.person_id as person_id,
 	38004268::integer as visit_concept_id, --Ambulatory Health Care Facilities, Clinic / Center, Oncology
@@ -30,6 +30,8 @@ from cte1 as t1
 left join cte1 as t2 on (t1.visit_occurrence_id - 1) = t2.visit_occurrence_id 
 and t1.person_id = t2.person_id;
 
+ALTER TABLE {TARGET_SCHEMA}.visit_occurrence SET TABLESPACE pg_default;
+
 alter table {TARGET_SCHEMA}.visit_occurrence add constraint xpk_visit_occurrence primary key (visit_occurrence_id) USING INDEX TABLESPACE pg_default;
 create index idx_visit_occ1 on {TARGET_SCHEMA}.visit_occurrence (person_id, visit_start_date, visit_source_value) TABLESPACE pg_default;
 CLUSTER {TARGET_SCHEMA}.visit_occurrence USING idx_visit_occ1;
@@ -40,7 +42,7 @@ CREATE INDEX idx_visit_concept_id ON {TARGET_SCHEMA}.visit_occurrence (visit_con
 --------------------------------
 drop table if exists {TARGET_SCHEMA}.visit_detail CASCADE;
 
-select 
+select distinct
 	t1.visit_detail_id, 
 	t1.person_id as person_id, 
 	38004268::integer as visit_detail_concept_id,	--Ambulatory Health Care Facilities, Clinic / Center, Oncology
@@ -65,7 +67,8 @@ from {SOURCE_SCHEMA}.temp_visit_detail as t1
 left join {SOURCE_SCHEMA}.temp_visit_detail as t2 on t1.visit_detail_id - 1 = t2.visit_detail_id 
 and t2.person_id = t1.person_id;
 
-	
+ALTER TABLE {TARGET_SCHEMA}.visit_detail SET TABLESPACE pg_default;
+
 ALTER TABLE {TARGET_SCHEMA}.visit_detail ADD CONSTRAINT xpk_visit_detail PRIMARY KEY (visit_detail_id) USING INDEX TABLESPACE pg_default;
 CREATE INDEX idx_visit_detail_person_id  ON {TARGET_SCHEMA}.visit_detail (person_id) TABLESPACE pg_default;
 CLUSTER {TARGET_SCHEMA}.visit_detail USING idx_visit_detail_person_id;
