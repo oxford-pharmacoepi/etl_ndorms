7902812cdb77958484e3d2f8b6193cda6d0db103
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-06-10 14:52:57 +0100

    Update 8a_gold_denom_drop.sql
    Moved out gold_eligibility

--- sql_scripts/8a_gold_denom_drop.sql
+++ sql_scripts/8a_gold_denom_drop.sql
@@ -1,3 +1,2 @@
 drop table if exists {SOURCE_SCHEMA}.gold_acceptable_pats CASCADE;
 drop table if exists {SOURCE_SCHEMA}.gold_allpractices CASCADE;
-drop table if exists {SOURCE_SCHEMA}.gold_eligibility CASCADE;
