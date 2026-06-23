8c3a62010c105038cdf038f5b055f2434cf0dbd5
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-06-23 12:14:47 +0100

    Update 1c_aurum_pk_idx_practice.sql

--- sql_scripts/1c_aurum_pk_idx_practice.sql
+++ sql_scripts/1c_aurum_pk_idx_practice.sql
@@ -1,4 +1,5 @@
 --PRACTICE
 alter table {SOURCE_SCHEMA}.practice add constraint pk_practice primary key (pracid) USING INDEX TABLESPACE pg_default;
+create index idx_practice on {SOURCE_SCHEMA}.practice(pracid) TABLESPACE pg_default;
+cluster {SOURCE_SCHEMA}.practice using idx_practice;
 create index idx_practice_region on {SOURCE_SCHEMA}.practice(region) TABLESPACE pg_default;
-cluster {SOURCE_SCHEMA}.practice using idx_practice_region;
