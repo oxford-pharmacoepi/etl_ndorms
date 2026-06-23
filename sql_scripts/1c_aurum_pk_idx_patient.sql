0b178113b205e3b965b775a8d76b77465072b9d8
Antonella Delmestri <antonella.delmestri@ndorms.ox.ac.uk>
2026-06-23 12:14:26 +0100

    Added cluster

--- sql_scripts/1c_aurum_pk_idx_patient.sql
+++ sql_scripts/1c_aurum_pk_idx_patient.sql
@@ -1,3 +1,7 @@
 --PATIENT
 alter table {SOURCE_SCHEMA}.patient add constraint pk_patient primary key (patid) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_patid ON {SOURCE_SCHEMA}.patient(patid ASC) TABLESPACE pg_default;
+CLUSTER {SOURCE_SCHEMA}.patient USING idx_patid;
+
 create index idx_patient_staffid on {SOURCE_SCHEMA}.patient(usualgpstaffid) TABLESPACE pg_default;
+
--- sql_scripts/1c_aurum_pk_idx_staff.sql
+++ sql_scripts/1c_aurum_pk_idx_staff.sql
@@ -1,3 +1,5 @@
 --STAFF
-alter table {SOURCE_SCHEMA}.staff add constraint pk_sfatt primary key (staffid) USING INDEX TABLESPACE pg_default;
+alter table {SOURCE_SCHEMA}.staff add constraint pk_staff primary key (staffid) USING INDEX TABLESPACE pg_default;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_staffid ON {SOURCE_SCHEMA}.staff(staffid ASC) TABLESPACE pg_default;
+CLUSTER {SOURCE_SCHEMA}.staff USING idx_staffid;
 create index idx_staff_jobid on {SOURCE_SCHEMA}.staff(jobcatid) TABLESPACE pg_default;
